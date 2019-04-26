659b4d555de1d5ea0605c823867e4af673996d21
Kirbyasdf <kirbyAugustus@gmail.com>
2019-04-26 08:13:03 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,9 @@
+
+
 require 'pry'
 class Song
   attr_accessor :name, :artist_name
+
   @@all = []
 
   def self.all
@@ -12,61 +15,71 @@ class Song
   end
 
   def self.create
+
     song = self.new
     song.save
-    song
+    return song
+
+
   end
 
-def self.new_by_name(song_name)
-  song = self.new
-  song.name = song_name
-  song
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+
+
+  end
+
+  def self.create_by_name(song_name)
+    # song = self.new
+    # song.name = song_name
+    # song.save
+    # song
+    song = self.new_by_name(song_name)
+    song.save
+    song
+
+
+  end
+
+def self.find_by_name(name)
+
+  @@all.find {|song| song.name == name}
 end
 
-def self.create_by_name(song_name)
-  song = self.new # => song = Song.new  instance of Song. song.class = Song
-  song.name = song_name # calls the attr_accessor in particular the setter method
-  song.save
-  song
+def self.find_or_create_by_name(name)
+  if  self.find_by_name(name) != nil
+      self.find_by_name(name)
+    else
+   self.create_by_name(name)
 end
-
-def self.find_by_name(song_name)
-@@all.each.find {|song| song.name == song_name}
-
-end
-
-def self.find_or_create_by_name(song_name)
-self.create_by_name(song_name)
-self.find_by_name(song_name)
 end
 
 def self.alphabetical
-  @@all.sort_by {|song| song.name}
+  self.all.sort_by { |song| song.name }
 end
 
 def self.new_from_filename(file_name)
-  file_name.slice! (-4..-1)
-  file = file_name.split(" - ")
-  song = self.new
-  song.name = file[1]
-  song.artist_name = file[0]
-  song
+file_name.slice!(".mp3")
+file_name_arr = file_name.split(" - ")
+song = self.new_by_name(file_name_arr[1])
+song.artist_name = file_name_arr[0]
+song
+
 end
 
 def self.create_from_filename(file_name)
-  file_name.slice! ".mp3"
-  file = file_name.split(" - ")
-  song = self.create
-   song.name = file[1]
-   song.artist_name = file[0]
-   song
+new_song = self.new_from_filename(file_name)
+new_song.save
+new_song
 
 end
 
 def self.destroy_all
   @@all.clear
 end
-
 
 
 end
