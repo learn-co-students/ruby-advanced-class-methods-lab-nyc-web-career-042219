d6c0a7faf454e01e314f0b2081302951668db46e
cbatsoula <cbatsoula@gmail.com>
2019-04-27 16:22:55 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,13 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def self.create
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
   def self.all
     @@all
   end
@@ -10,4 +17,51 @@ class Song
     self.class.all << self
   end
 
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.create ##using the self.create uses that shoveling so I dont need to write the same exact code over again
+    song.name = song_name
+    song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.detect { |song| song.name == song_name }
+    end
+
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name }
+  end
+
+  def self.new_from_filename(filename)
+    new_filename = filename.split(" - ")
+    artist_name = new_filename[0]
+    song_name = new_filename[1].gsub(".mp3","")
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    new_filename = filename.split(" - ")
+    artist_name = new_filename[0]
+    song_name = new_filename[1].gsub(".mp3", "")
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
